1eb12df03587f783f8ef0d0f9367f91334043905
Kalil de Lima <kaozdl@gmail.com>
2018-12-25 22:48:59 -0300

    add models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/Calendar'
+  get 'user/Event'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user do
+    resources :calendar do
+      resources :event
+    end
+  end
 end
